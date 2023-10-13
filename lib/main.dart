2cad84d26fc37ae75d23ed1eacff132e60693728
Chris Norman CRCB <chris@crcb.org.uk>
2023-10-13 15:05:23 +0100

    Ensure semantics.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
+  SemanticsBinding.instance.ensureSemantics();
 }
 
 class MyApp extends StatelessWidget {
